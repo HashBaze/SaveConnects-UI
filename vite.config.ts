56c22a4c93aa90ae4b9c454e5cf601f2cb398dfa
Tishan Shamika <133506972+Tishan-001@users.noreply.github.com>
2024-09-09 20:52:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,5 @@ export default defineConfig({
  preview: {
   port: 8080,
   strictPort: true,
- },
- server: {
-  port: 8080,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:8080",
- },
+ }
 });
